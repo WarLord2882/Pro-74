6c66bf5e3893e2e0c006c36d2395090e0f291d3d
WarLord2287 <77100801+WarLord2882@users.noreply.github.com>
2022-06-06 18:25:46 +0530

    Update RideHistory.js

--- RideHistory.js
+++ RideHistory.js
@@ -166,32 +166,7 @@ export default class RideHistoryScreen extends Component {
             onEndReachedThreshold={0.7}
           />
 
-              {/* <FlatList
-              data=allTransactions
-              renderItem=this.renderItem
-              keyExtractor={(item, index) => index.toString()}
-              onEndReached={() => 
-                this.fetchMoreTransactions(searchText)}
-              onEndReachedThreshold={0.7}
-            /> */}
 
-           {/* <FlatList
-            data:{allTransactions}
-            renderItem:{this.renderItem}
-            keyExtractor:{(item, index) => index.toString()}
-            onEndReached:{() => 
-              this.fetchMoreTransactions(searchText)}
-            onEndReachedThreshold={0.7}
-          /> */}
-
-            <FlatList
-            data={"allTransactions"}
-            renderItem={"this.renderItem"}
-            keyExtractor={(item, index) => index.toString()}
-            onEndReached=
-            {this.fetchMoreTransactions(searchText)}
-            onEndReachedThreshold={0.7}
-          /> 
 
         </View>
       </View>
